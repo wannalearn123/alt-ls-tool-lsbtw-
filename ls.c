c9eb29d71fcd8c19f995bc2e627deb269369aa7f
wannalearn123 <nurdiansyahagung277@gmail.com>
2026-07-30 13:20:48 +0700

    add helper for revealing user or group name (not completed)

--- ls.c
+++ ls.c
@@ -3,8 +3,30 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-void mode_string(mode_t mode, char *star) {
+void mode_string(mode_t mode, char *str) {
+	if (S_ISDIR(mode))  	str[0] = 'd';
+	else if (S_ISLNK(mode)) str[0] = 'l';
+	else if (S_ISCHR(mode)) str[0] = 'c';
+	else if (S_ISBLK(mode)) str[0] = 'b';
+	else if (S_ISFIFO(mode)) str[0] = 'p';
+	else if (S_ISSOCK(mode)) str[0] = 's';
+	else 		         	str[0] = '-';
 
+	str[1] = (mode & S_IRUSR) ? 'r' : '-';
+	str[2] = (mode & S_IWUSR) ? 'w' : '-';
+	str[3] = (mode & S_IXUSR) ? 'x' : '-';
+	str[4] = (mode & S_IRUSR) ? 'r' : '-';
+	str[5] = (mode & S_IWGRP) ? 'w' : '-';
+	str[6] = (mode & S_IXGRP) ? 'x' : '-';
+	str[7] = (mode & S_IROTH) ? 'r' : '-';
+	str[8] = (mode & S_IWOTH) ? 'w' : '-';
+	str[9] = (mode & S_IXOTH) ? 'x' : '-';
+	str[10] = '\0';
+}
+
+void print_long(const char *dir, const char *name) {
+	char fullpath[4096];
+	snprintf(fullpath, sizeof(fullpath), "%s/%s", dir, name);
 }
 
 int show_all = 0;
